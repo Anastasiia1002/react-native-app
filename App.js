c77f26ec1aa5ba0c4f5d066ed2b8b9abb6dd1f91
Anastasiia <106449970+Anastasiia1002@users.noreply.github.com>
2023-01-05 14:45:00 +0200

    start

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Kukusikyk</Text>
+      <Text>Kukusiky</Text>
      
       <StatusBar style="auto" />
     </View>
